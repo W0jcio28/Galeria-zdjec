20d068f7b1b9b17d0fe4710ae3c04ab88227cf03
W0jcio28 <168183232+W0jcio28@users.noreply.github.com>
2024-05-13 16:38:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const ssaki = document.querySelector('ssaki')
+const ptaki = document.querySelector('ptaki')
+const ryby = document.querySelector('ryby')
+
+const zdjeciaSsakow = () => {
+	window.open("ssaki")
+}
+ssaki.addEventListener('click', () => {
+	zdjeciaSsakow()
+})
